fd886540ce85c77e8aa5b5041b09817fb8810c00
djuniow <138530826+djuniow@users.noreply.github.com>
2024-05-21 22:56:20 -0300

    Update Endereços.py

--- Endereços.py
+++ Endereços.py
@@ -23,7 +23,7 @@ name, authenticator_status, username = authenticator.login()
 
 @st.cache_resource
 def carregamento_uni():
-    df_unidades = pd.read_excel('dados/endereco_unidades.xlsx')
+    df_unidades = pd.read_excel(r'dados\endereco_unidades.xlsx')
     df_unidades_lista = df_unidades['UNIDADE:'].drop_duplicates()
 
     return df_unidades_lista , df_unidades
@@ -40,12 +40,14 @@ if authenticator_status:
     endereco_unidade = endereco_unidade.reset_index()
     lat_unidade = endereco_unidade['LATITUDE:'][0]
     log_unidade = endereco_unidade['LONGITUDE:'][0]
+    nome_unidade = str(endereco_unidade["UNIDADE:"][0]).replace(' ', '2%')
+    end_unidade = str(endereco_unidade["CEP:"][0]).replace(' ', '2%')
     st.subheader(endereco_unidade['UNIDADE:'][0])
     st.write('Endereço:')
     st.write(endereco_unidade['CEP:'][0])
 
     st.write('Compartilhar')
-    st.markdown(f'[![Foo](https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/50px-WhatsApp.svg.png)](https://api.whatsapp.com/send?text="{str(endereco_unidade["UNIDADE:"][0])}{str(endereco_unidade["CEP:"][0])}")')
+    st.markdown(f'[![Foo](https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/50px-WhatsApp.svg.png)](https://api.whatsapp.com/send?text="{nome_unidade}2%{end_unidade })')
 elif authenticator_status == False:
     st.error('Usuário errado')
 elif authenticator_status == None:
