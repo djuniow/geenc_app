9a7898bc973edbdab609b379c1338d9d3ac0a2e6
djuniow <138530826+djuniow@users.noreply.github.com>
2024-05-25 21:51:08 -0300

    Update Endereços.py

--- Endereços.py
+++ Endereços.py
@@ -48,7 +48,7 @@ if authenticator_status:
     st.write(endereco_unidade['CEP:'][0])
 
     st.write('Compartilhar')
-    st.markdown(f'[![Foo](https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/50px-WhatsApp.svg.png)](https://api.whatsapp.com/send?text=teste%0teste)')
+    st.markdown(f'[![Foo](https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/50px-WhatsApp.svg.png)](https://api.whatsapp.com/send?text=jjcjcj)')
 elif authenticator_status == False:
     st.error('Usuário errado')
 elif authenticator_status == None:
