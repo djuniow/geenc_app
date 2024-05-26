f0ef61f527feaa31ea771ea054f1e8dac9c1ff2e
djuniow <138530826+djuniow@users.noreply.github.com>
2024-05-25 22:53:56 -0300

    Update Endereços.py

--- Endereços.py
+++ Endereços.py
@@ -42,7 +42,7 @@ if authenticator_status:
     log_unidade = endereco_unidade['LONGITUDE:'][0]
     nome_unidade = str(endereco_unidade["UNIDADE:"][0]).replace(' ', '%20')
     end_unidade = str(endereco_unidade["CEP:"][0]).replace(' ', '%20')
-    texto = str('UNIDADE'+'%20%0,'+nome_unidade+'%20%0'+'ENDEREÇO'+'%20%0'+end_unidade)
+    texto = str('UNIDADE'+'%20%0A'+nome_unidade+'%20%0'+'ENDEREÇO'+'%20%0'+end_unidade)
     st.subheader(endereco_unidade['UNIDADE:'][0])
     st.write('Endereço:')
     st.write(endereco_unidade['CEP:'][0])
