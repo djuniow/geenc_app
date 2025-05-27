f1f0281295487ce09abbae86bcc26dd2d1cf3911
81140657 <djalmaluis@correios.com>
2025-05-27 11:11:20 -0300

    implementação da pagina de prismas

--- pages/02_Prismas.py
+++ pages/02_Prismas.py
@@ -1,2 +1,17 @@
 import streamlit as st
+import pandas as pd
 st.title('Prismas')
+
+
+data = pd.read_excel('dados/prismas.xlsx')
+
+opcoes = pd.concat([data['PRISMA'],data['UNIDADE']])
+
+busca = st.selectbox('Selecione o prisma ou unidade', options=opcoes)
+
+if type(busca) is int:
+    data_busca = data.loc[data['PRISMA']==busca]
+    st.table(data_busca)
+else:
+    data_busca = data.loc[data['UNIDADE']== busca]
+    st.table(data_busca[:])
