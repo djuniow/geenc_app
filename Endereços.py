bc35a663afa851a74141401c4edb2cf34e5a87a2
81140657 <djalmaluis@correios.com>
2025-05-28 14:42:40 -0300

    correção do menu

--- Endereços.py
+++ Endereços.py
@@ -7,7 +7,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.markdown(
         """<a href="https://idugeenc.streamlit.app/Conexões">
-        <img src="data:image/png;base64,{}" width="25">
+        <img src="data:image/png;base64,{}" width="65">
         </a>""".format(
             base64.b64encode(open("images/mapa.png", "rb").read()).decode()
         ),
@@ -15,7 +15,7 @@ with col1:
     )
     st.markdown(
         """<a href="https://idugeenc.streamlit.app/Endereços">
-        <img src="data:image/jpg;base64,{}" width="25">
+        <img src="data:image/jpg;base64,{}" width="65">
         </a>""".format(
             base64.b64encode(open("images/endereços.jpg", "rb").read()).decode()
         ),
