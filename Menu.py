45f06c5c686662a0ac68889cdbedebb794b7977f
81140657 <djalmaluis@correios.com>
2025-05-28 13:36:16 -0300

    carregmanto da pagina de endereços e arquivos diversos

---
+++ Menu.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import base64
+
+col1, col2 = st.columns(2)
+
+
+with col1:
+    st.markdown(
+        """<a href="https://idugeenc.streamlit.app/Conexões">
+        <img src="data:image/png;base64,{}" width="25">
+        </a>""".format(
+            base64.b64encode(open("images/mapa.PNG", "rb").read()).decode()
+        ),
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        """<a href="https://idugeenc.streamlit.app/Endereços">
+        <img src="data:image/jpg;base64,{}" width="25">
+        </a>""".format(
+            base64.b64encode(open("images/endereços.jpg", "rb").read()).decode()
+        ),
+        unsafe_allow_html=True,
+    )
