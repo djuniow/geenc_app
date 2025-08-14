19e4a625594a3042fed611b67d130d493f8f66d8
81140657 <djalmaluis@correios.com>
2025-08-14 10:30:03 -0300

    Acertado a mensagem da contingencia

--- pages/05_Rastreamento.py
+++ pages/05_Rastreamento.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import rastro
+from camera_input_live import camera_input_live
 
 num_obj = st.text_input(label="Objeto")
+image = camera_input_live()
+
+if image:
+  st.image(image)
 
 if st.button('Pesquisar'):
     resultado = rastro.get_postagem(data=num_obj)
