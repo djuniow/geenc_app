908c5d7f056862cde74d52997b09f77b5b579749
81140657 <djalmaluis@correios.com>
2025-08-14 10:52:31 -0300

    Acertado a mensagem da contingencia

--- pages/05_Rastreamento.py
+++ pages/05_Rastreamento.py
@@ -3,7 +3,7 @@ import rastro
 from streamlit_qrcode_scanner import qrcode_scanner
 
 num_obj = st.text_input(label="Objeto")
-qr_code = qrcode_scanner(key='qrcode_scanner')
+qr_code = qrcode_scanner()
 
 if qr_code:
   st.write(qr_code)
